623d6da9e0d619636d88cbef55b623de5584ae24
superyngo <superyngo@gmail.com>
2023-08-13 14:25:57 +0800

    UPDATE

--- src/routes/workshop/+page.svelte
+++ src/routes/workshop/+page.svelte
@@ -1,5 +1,5 @@
 <div class="wrapper">
-	<ol>
-		<li><a href="/workshop/snake">snake</a></li>
-	</ol>
+  <ol>
+    <!-- <li><a href="/workshop/snake">snake</a></li> -->
+  </ol>
 </div>
--- src/routes/workshop/snake/+page.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-  import {Snake} from "$lib/apps";
-</script>
-
-<div class="appWrapper h-full relative">
-  <Snake canvasWidth={10} canvasHeight={20} speed={3} />
-</div>
