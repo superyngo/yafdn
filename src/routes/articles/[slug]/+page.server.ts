a25328986e63b4ab8dee8eee40a19a5ea2fc54ce
superyngo <superyngo@gmail.com>
2023-08-13 20:34:14 +0800

    UPDATE

--- src/routes/articles/[slug]/+page.server.ts
+++ src/routes/articles/[slug]/+page.server.ts
@@ -1,9 +1,11 @@
+console.log("step1");
+
 import {createPool} from "@vercel/postgres";
 import {error} from "@sveltejs/kit";
 const db = createPool();
 
 import {renderMd} from "$lib/components/mdsvexCustom/renderMd";
-
+console.log("step2", renderMd, createPool);
 export async function load({params}) {
   try {
     const {
@@ -17,10 +19,11 @@ export async function load({params}) {
         [params.slug]
       )
     ).rows[0];
+    console.log("step3", mdraw);
     const html = await renderMd(mdraw);
     metadata.categories = categories;
     return {html, metadata};
   } catch (e) {
-    throw error(404, `Can't find page ${params.slug} `);
+    throw error(404, `Can't find page ${params.slug} ${renderMd} ${mdraw} `);
   }
 }
