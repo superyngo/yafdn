8054999c9f01416d4211f10e5447bd79bca0a53c
superyngo <superyngo@gmail.com>
2023-08-14 13:40:16 +0800

    UPDATE

--- src/routes/articles/article/[slug]/+page.server.ts
+++ src/routes/articles/article/[slug]/+page.server.ts
@@ -15,7 +15,7 @@ export async function load({params}) {
         [params.slug]
       )
     ).rows[0];
-    const html = mdraw; //await renderMd(mdraw);
+    const html = await renderMd(mdraw);
     metadata.categories = categories;
     return {html, metadata, mdraw};
   } catch (e) {
