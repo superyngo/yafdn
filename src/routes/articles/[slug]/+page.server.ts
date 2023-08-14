a1bffeac9e979f1c1f5ede0c6010f6076056daf6
superyngo <superyngo@gmail.com>
2023-08-14 09:03:33 +0800

    UPDATE

--- src/routes/articles/[slug]/+page.server.ts
+++ src/routes/articles/[slug]/+page.server.ts
@@ -1,7 +1,31 @@
+import {compile, escapeSvelte} from "mdsvex";
+import remarkUnwrapImages from "remark-unwrap-images";
+import remarkToc from "remark-toc";
+import rehypeSlug from "rehype-slug";
+import {getHighlighter} from "shiki";
+
+const mdsvexoption = {
+  highlight: {
+    highlighter: async (code: string, lang = "text") => {
+      const highlighter = await getHighlighter({
+        theme: "github-dark",
+      });
+      const html = escapeSvelte(highlighter.codeToHtml(code, {lang}));
+      return `${html}`;
+    },
+  },
+  remarkPlugins: [remarkUnwrapImages, [remarkToc, {tight: true}]],
+  rehypePlugins: [rehypeSlug],
+};
+
+async function renderMd(mdraw: string) {
+  return (await compile(mdraw, mdsvexoption)).code;
+}
+
 import {createPool} from "@vercel/postgres";
 import {error} from "@sveltejs/kit";
 const db = createPool();
-import {renderMd} from "/src/lib/components/mdsvexCustom/renderMd";
+// import {renderMd} from "/src/lib/components/mdsvexCustom/renderMd";
 export async function load({params}) {
   try {
     const {
