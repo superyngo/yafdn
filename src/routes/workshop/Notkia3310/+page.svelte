6cff9561557316c94554fc37ff5d56cc506d621f
superyngo <superyngo@gmail.com>
2023-08-14 21:57:02 +0800

    UPDATE

--- src/routes/workshop/Notkia3310/+page.svelte
+++ src/routes/workshop/Notkia3310/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import Nikia3310 from "$lib/apps/nikia3310.svelte";
+  import Snake3310 from "$lib/apps/snake3310.svelte";
 </script>
 
-<Nikia3310 canvasWidth={30} canvasHeight={20} speed={3} controlHeight="0px" />
+<Nikia3310 />
