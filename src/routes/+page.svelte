0f95ce475e1410ca6aa964f33f4154c68c31eb89
superyngo <superyngo@gmail.com>
2023-08-15 13:51:08 +0800

    UPDATE

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
   import Home from "./home.md";
 </script>
 
-<h1 class="text-4xl leading-loose">歡迎</h1>
+<h1 class="text-4xl leading-loose animate-bounce">歡迎</h1>
 <div class="prose mx-auto">
   <Home />
 </div>
