902da52d96da21f2b7aa6ad2aa7318551efee814
superyngo <superyngo@gmail.com>
2023-09-05 13:41:58 +0800

    test_webhook

--- src/routes/projects/testApiWebhook/+server.ts
+++ src/routes/projects/testApiWebhook/+server.ts
@@ -15,6 +15,6 @@ export async function GET(request) {
 export async function POST({request}) {
   const requestBody = await request.text();
   const jsonData = JSON.parse(requestBody);
-  console.log("jsonData", Object.keys(jsonData));
+  console.log("action", jsonData.action);
   return json(jsonData);
 }
